9809f944e85c5dc8e42dd8d61f661c5174d6fe20
aSondergaard <andrewsondergaard@gmail.com>
2018-09-14 08:10:16 +0200

    this is a simple calculator with a GUI and made with Windows Forms and C#/.NET. It can only add, divide, multiply and subtract

---
+++ Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.Calculate = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.AccessibleName = "txtNr1";
+            this.textBox1.Location = new System.Drawing.Point(49, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(236, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.AccessibleName = "txtNr2";
+            this.textBox2.Location = new System.Drawing.Point(49, 67);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(236, 20);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // Calculate
+            // 
+            this.Calculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Calculate.Location = new System.Drawing.Point(49, 162);
+            this.Calculate.Name = "Calculate";
+            this.Calculate.Size = new System.Drawing.Size(236, 35);
+            this.Calculate.TabIndex = 2;
+            this.Calculate.Text = "Calculate";
+            this.Calculate.UseVisualStyleBackColor = true;
+            this.Calculate.Click += new System.EventHandler(this.Calculate_Click);
+            // 
+            // label1
+            // 
+            this.label1.AccessibleName = "lblResult";
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(80, 301);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 31);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Waiting...";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Add",
+            "Subtract",
+            "Divide",
+            "Multiply"});
+            this.comboBox1.Location = new System.Drawing.Point(49, 93);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(236, 21);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(350, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(419, 31);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Andrews first simple calculator \\o/";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Calculate);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button Calculate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Calculate_Click(object sender, EventArgs e)
+        {
+            int result = 0;
+            //Write the code
+            int nr1 = 0; //int.Parse(textBox1.Text);
+            int nr2 = 0; //int.Parse(textBox2.Text);
+            try
+            {
+                nr1 = int.Parse(textBox1.Text);
+                nr2 = int.Parse(textBox2.Text);
+            }
+            catch { }
+
+            //this "try" makes sure that if no operator is chosen, the default is "Add" and we avoid an error/crash
+            string option = "Add";
+            try
+            {
+                option = comboBox1.SelectedItem.ToString();
+            }
+            catch { }
+
+            switch (comboBox1.SelectedItem.ToString())
+            {
+                case "Add":
+                    result = nr1 + nr2;
+                    break;
+                case "Subtract":
+                    result = nr1 - nr2;
+                    break;
+                case "Divide":
+                    result = nr1 / nr2;
+                    break;
+                case "Multiply":
+                    result = nr1 * nr2;
+                    break;
+                default:
+                    Console.WriteLine("Default case");
+                    break;
+            }
+
+            if (result == 0)
+                label1.Text = "That is not a number";
+            else
+                label1.Text = "The result is " + result;
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+  
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
